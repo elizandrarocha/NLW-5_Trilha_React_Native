084bc040e4c1a9b8f17ca55b1dd1419d1fc832f2
Elizandra Rocha <rochaelizandraf@gmail.com>
2021-04-22 12:08:03 -0400

    inclusao expo-google-fonts; +refatoração

--- plantmanager/App.tsx
+++ plantmanager/App.tsx
@@ -1,12 +1,28 @@
 //import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Welcome } from './src/pages/Welcome';
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost';
+import AppLoading from 'expo-app-loading'
 //import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [fonstLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if (!fonstLoaded)
+    return <AppLoading />
+
   return (
     <Welcome />
-  );
+  )
+
 }
 
 /*
